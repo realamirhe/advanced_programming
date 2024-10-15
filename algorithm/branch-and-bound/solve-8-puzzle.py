9a88e40b143ab97cee95dbf03dc5e4e48686516b
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2024-10-15 12:53:30 +0330

    feat: :sparkles: Add B&B algorithms implementation

--- algorithm/branch-and-bound/solve-8-puzzle.py
+++ algorithm/branch-and-bound/solve-8-puzzle.py
@@ -1,5 +1,88 @@
+from __future__ import annotations
+
+import copy
+import heapq as hq
+
+
+class Pos:
+    __slots__ = ["i", "j"]
+
+    def __init__(self, row: int, col: int):
+        self.i = row
+        self.j = col
+
+    def __add__(self, other: Pos):
+        assert isinstance(other, Pos), f"{other} must be instance of Pos"
+        return Pos(self.i + other.i, self.j + other.j)
+
+    def __sub__(self, other: Pos):
+        assert isinstance(other, Pos), f"{other} must be instance of Pos"
+        return Pos(self.i - other.i, self.j - other.j)
+
+    def is_safe(self, n: int):
+        return 0 <= self.i < n and 0 <= self.j < n
+
+
+class Node:
+    __slots__ = ["parent", "state", "pos", "cost", "final", "level"]
+
+    def __init__(
+        self,
+        /,
+        *,
+        parent: Node,
+        state: list[list[int]],
+        final: list[list[int]],
+        pos: Pos,
+    ):
+        self.parent = parent
+        self.state = state
+        self.final = final
+        self.pos = pos  # position for zero
+        self.level = parent.level + 1 if parent else 0
+        self.cost = self.calc_cost()
+
+    def calc_cost(self) -> int:
+        n = len(self.state)
+        count = 0
+        for i in range(n):
+            for j in range(n):
+                count += int(self.state[i][j] and self.state[i][j] != self.final[i][j])
+        return count
+
+    def new_node(self, move: Pos) -> Node:
+        state = copy.deepcopy(self.state)  # new state
+        pos = self.pos + move  # new pos
+
+        if not pos.is_safe(len(state)):
+            return False
+
+        state[pos.i][pos.j], state[self.pos.i][self.pos.j] = (
+            state[self.pos.i][self.pos.j],
+            state[pos.i][pos.j],
+        )
+
+        return Node(parent=self, state=state, final=self.final, pos=pos)
+
+    def print_state(self) -> None:
+        for row in self.state:
+            print(" ".join(map(str, row)))
+
+    def __lt__(self, other: Node):
+        return self.cost < other.cost
+
+
+def printPath(root: Node):
+    if root is None:
+        return
+
+    printPath(root.parent)
+    root.print_state()
+    print()
+
+
 def solve_8_puzzle(
-    start_state: list[list[int]], goal_state: list[list[int]]
+    initial: list[list[int]], final: list[list[int]], pos_z: Pos
 ) -> list[str]:
     """
     Function to solve the 8-Puzzle Problem using search algorithms like A* or BFS.
@@ -28,4 +111,26 @@ def solve_8_puzzle(
     >>> solve_8_puzzle(start_state, goal_state)
     ['right', 'down', 'left']  # Example solution steps
     """
-    ...
+
+    root = Node(parent=None, state=initial, final=final, pos=pos_z)
+    actions = [root]
+    possible_moves = [Pos(1, 0), Pos(0, -1), Pos(-1, 0), Pos(0, 1)]
+    # hq.heappush(actions, )
+    while actions:
+        node_min = hq.heappop(actions)
+        if node_min.cost == 0:
+            return printPath(node_min)
+
+        for move in possible_moves:
+            if new_child := node_min.new_node(move):
+                hq.heappush(actions, new_child)
+
+
+initial = [[1, 2, 3], [5, 6, 0], [7, 8, 4]]
+final = [[1, 2, 3], [5, 8, 6], [0, 7, 4]]
+
+solve_8_puzzle(
+    initial=initial,
+    final=final,
+    pos_z=Pos(1, 2),
+)
--- algorithm/branch-and-bound/solve-n-queen.py
+++ algorithm/branch-and-bound/solve-n-queen.py
@@ -1,3 +1,48 @@
+class LookupCodes:
+    def __init__(self, n) -> None:
+        self.n = n
+        self.slash_code = [False] * (2 * n - 1)
+        self.back_slash_code = [False] * (2 * n - 1)
+        self.rows = [False] * n
+
+    def is_safe(self, i, j):
+        return not (
+            self.slash_code[i + j]
+            or self.back_slash_code[i - j + (self.n - 1)]
+            or self.rows[i]
+        )
+
+    def fill(self, i, j, value=True):
+        self.slash_code[i + j] = value
+        self.back_slash_code[i - j + self.n - 1] = value
+        self.rows[i] = value
+
+
+EMPTY_STATE = "⬛"
+FILLED_STATE = "🔳"
+
+
+def solve_n_queens_util(mat, lookup, col):
+    if col == lookup.n:
+        return True
+
+    for row in range(lookup.n):
+        if lookup.is_safe(row, col):
+            lookup.fill(row, col, True)
+            mat[row][col] = FILLED_STATE
+            if solve_n_queens_util(mat, lookup, col + 1):
+                return True
+            mat[row][col] = EMPTY_STATE
+            lookup.fill(row, col, False)
+
+    return False
+
+
+def pprint(mat):
+    for row in mat:
+        print(" ".join(list(map(str, row))))
+
+
 def solve_n_queens(n: int) -> list[list[str]]:
     """
     Function to solve the N-Queens problem using backtracking.
@@ -26,4 +71,14 @@ def solve_n_queens(n: int) -> list[list[str]]:
      ['..Q.', 'Q...', '...Q', '.Q..']]
     # Two valid solutions for 4-Queens problem
     """
-    ...
+    lookup = LookupCodes(n)
+    mat = [[EMPTY_STATE] * n for i in range(n)]
+
+    col = 0
+    if solve_n_queens_util(mat, lookup, col):
+        pprint(mat)
+    else:
+        print("no answer")
+
+
+solve_n_queens(18)
